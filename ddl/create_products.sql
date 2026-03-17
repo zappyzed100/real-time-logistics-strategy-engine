8c806f9bea1c014120f620d00038b7d7f2d381df
zappyzed100 <zappyzed100@gmail.com>
2026-03-17 23:59:41 +0900

    Revert "feat: 基本4テーブルのDDL作成 (#7)"
    This reverts commit 5ccc75a05a306f64a9f1e2c7e8a3e97332906c59.

--- ddl/create_products.sql
+++
@@ -1,8 +0,0 @@
--- 商品マスターテーブルの作成
-CREATE TABLE IF NOT EXISTS products (
-    product_id   INT PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
-    category     VARCHAR(100),
-    weight_kg    FLOAT COMMENT '配送コスト計算に使用',
-    unit_price   NUMBER(12, 2)
-);
